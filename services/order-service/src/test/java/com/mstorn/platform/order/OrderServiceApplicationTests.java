7d448ce982f410c4fa8063a0aa038216ef0c7888
Marc Stornfels <ms@fast-forward-it.de>
2026-03-04 21:02:13 +0100

    feat: initial order-service spring boot microservice

---
+++ services/order-service/src/test/java/com/mstorn/platform/order/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mstorn.platform.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
